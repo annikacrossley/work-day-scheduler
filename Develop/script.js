6e115d951e77b15bdcd7cc00be37ec2847bf3831
annikacrossley <132411887+annikacrossley@users.noreply.github.com>
2023-07-05 14:00:59 -0500

    save button fxn and for loop for local storage

--- Develop/script.js
+++ Develop/script.js
@@ -4,16 +4,30 @@
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
+  // local storage. 
+  //HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  $(".saveBtn").on('click', function(){
+    let time = $(this).attr('id')
+    let text = $(this).siblings('.description').val()
+    localStorage.setItem(time, text)
+  })
+
+  for(let i=9; i < 18; i++) {
+    $(`#${i}`).siblings('.description').val(localStorage.getItem(i))
+  }
+  
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+  $(".time-block").each(function(){
+    let currentHr;
+    let blockHr = $(this).children('.saveBtn').attr('id');
+  })
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
@@ -21,3 +35,16 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+
+
+
+
+
+
+//WHEN I open the planner THEN the current day is displayed at the top of the calendar
+//WHEN I scroll down THEN I am presented with time blocks for standard business hours of 9am to 5pm
+//WHEN I view the time blocks for that day THEN each time block is color-coded to indicate whether it is in the past, present, or future
+//WHEN I click into a time block THEN I can enter an event
+//WHEN I click the save button for that time block THEN the text for that event is saved in local storage
+//WHEN I refresh the page THEN the saved events persist
